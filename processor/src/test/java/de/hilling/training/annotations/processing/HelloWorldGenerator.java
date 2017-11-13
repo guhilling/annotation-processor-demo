ba4db8f014b585b0db56ae14560e522c6a2894d6
Gunnar Hilling <gunnar@hilling.de>
2017-11-13 21:20:49 +0100

    - cleanup.

---
+++ processor/src/test/java/de/hilling/training/annotations/processing/HelloWorldGenerator.java
@@ -0,0 +1,32 @@
+package de.hilling.training.annotations.processing;
+
+import javax.lang.model.element.Modifier;
+
+import org.junit.Test;
+
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.TypeSpec;
+
+public class HelloWorldGenerator {
+
+    @Test
+    public void generateHelloWorld() throws Exception {
+        MethodSpec main = MethodSpec.methodBuilder("main")
+                                    .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
+                                    .returns(void.class)
+                                    .addParameter(String[].class, "args")
+                                    .addStatement("$T.out.println($S)", System.class, "Hello, JavaPoet!")
+                                    .build();
+
+        TypeSpec helloWorld = TypeSpec.classBuilder("HelloWorld")
+                                      .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
+                                      .addMethod(main)
+                                      .build();
+
+        JavaFile javaFile = JavaFile.builder("com.example.helloworld", helloWorld)
+                                    .build();
+
+        javaFile.writeTo(System.out);
+    }
+}
